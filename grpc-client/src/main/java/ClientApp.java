1902b08d606380f189d01c459a9c838554cfbf55
alwayswanna <a.gleb@yahoo.com>
2021-09-02 21:51:31 +0500

    + send image;

--- grpc-client/src/main/java/ClientApp.java
+++ grpc-client/src/main/java/ClientApp.java
@@ -1,25 +1,110 @@
 import a.gleb.service.GreetingServiceGrpc;
 import a.gleb.service.Service;
+import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+
+import java.io.*;
+import java.util.concurrent.TimeUnit;
 
 public class ClientApp {
 
-    public static void main(String[] args) {
-        ManagedChannel channel = ManagedChannelBuilder.forTarget("localhost:50051")
+    private static final int PORT = 50051;
+
+    private final ManagedChannel channel;
+    private final GreetingServiceGrpc.GreetingServiceBlockingStub stub;
+    private final GreetingServiceGrpc.GreetingServiceStub async_stub;
+
+
+    public ClientApp(String host, int port) {
+        this(ManagedChannelBuilder.forAddress(host, port)
                 .usePlaintext()
-                .build();
+                .build());
+    }
 
-        GreetingServiceGrpc.GreetingServiceBlockingStub stub = GreetingServiceGrpc.newBlockingStub(channel);
+    ClientApp(ManagedChannel channel) {
+        this.channel = channel;
+        stub = GreetingServiceGrpc.newBlockingStub(channel);
+        async_stub = GreetingServiceGrpc.newStub(channel);
 
+    }
+
+    public void shutdown() throws InterruptedException {
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    public void startStream(final String filepath) {
+        StreamObserver<Service.ImageResponse> responseObserver = new StreamObserver<Service.ImageResponse>() {
+            @Override
+            public void onNext(Service.ImageResponse value) {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+
+            }
+        };
+
+        StreamObserver<Service.ImageRequest> requestObserver = async_stub.uploadImage(responseObserver);
+        try {
+            File file = new File(filepath);
+            if (!file.exists()) {
+                System.out.println("File does not exist");
+                return;
+            }
+            try {
+                BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+                int buffer_size = 512 * 1024;
+                byte[] buffer = new byte[buffer_size];
+                int size = 0;
+
+                while ((size = bufferedInputStream.read(buffer)) > 0) {
+                    ByteString byteString = ByteString.copyFrom(buffer, 0, size);
+                    var request = Service.ImageRequest.newBuilder().setName(filepath).setData(byteString).setOffset(size).build();
+                    requestObserver.onNext(request);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (RuntimeException rE) {
+            rE.printStackTrace();
+            throw rE;
+        }
+        requestObserver.onCompleted();
+    }
+
+    private void sendSimpleRequest() {
         Service.HelloRequest request = Service.HelloRequest
                 .newBuilder()
-                .setName("ClientName")
+                .setName("client_name")
                 .build();
 
         Service.HelloResponse response = stub.greeting(request);
-        System.out.println("Client send request. Returned response: " + response);
+        System.out.println(response);
 
-        channel.shutdownNow();
     }
+
+    public static void main(String[] args) throws InterruptedException {
+        ClientApp clientApp = new ClientApp("localhost", 50051);
+        try {
+            clientApp.startStream("C:\\Users\\agleb\\Pictures\\test.png");
+            clientApp.sendSimpleRequest();
+        } finally {
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            clientApp.shutdown();
+        }
+
+    }
+
 }
--- grpc-server/src/main/java/rpc/ServiceRpcImpl.java
+++ grpc-server/src/main/java/rpc/ServiceRpcImpl.java
@@ -4,11 +4,19 @@ import a.gleb.service.GreetingServiceGrpc;
 import a.gleb.service.Service;
 import io.grpc.stub.StreamObserver;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class ServiceRpcImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+    private final int mStatus = 200;
+    private final String message = "ImageUpload: DONE";
+    private BufferedOutputStream bufferedOutputStream = null;
+
 
     @Override
     public void greeting(Service.HelloRequest request, StreamObserver<Service.HelloResponse> responseObserver) {
-        System.out.println(request);
+        System.out.println("greeting request: " + request);
 
         Service.HelloResponse response = Service.HelloResponse.newBuilder()
                 .setGreeting("Response from server to: " + request.getName())
@@ -17,5 +25,53 @@ public class ServiceRpcImpl extends GreetingServiceGrpc.GreetingServiceImplBase 
         responseObserver.onNext(response);
 
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public StreamObserver<Service.ImageRequest> uploadImage(StreamObserver<Service.ImageResponse> responseObserver) {
+        int count = 0;
+        return new StreamObserver<Service.ImageRequest>() {
+            @Override
+            public void onNext(Service.ImageRequest request) {
+                byte[] data = request.getData().toByteArray();
+                long offset = request.getOffset();
+                String name = request.getName();
+                System.out.println(name);
+                try {
+                    if (bufferedOutputStream == null) {
+                        bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("D:\\test.png"));
+                    }
+                    bufferedOutputStream.write(data);
+                    bufferedOutputStream.flush();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onNext(Service.ImageResponse
+                        .newBuilder()
+                        .setStatus(mStatus)
+                        .setMessage(message)
+                        .build()
+                );
+                responseObserver.onCompleted();
+
+                try {
+                    bufferedOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    bufferedOutputStream = null;
+                }
+            }
+        };
     }
 }
