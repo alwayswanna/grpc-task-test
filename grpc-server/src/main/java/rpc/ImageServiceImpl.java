0e5e7c2376d61fb5bd14f3236d93c52e8214be9d
alwayswanna <a.gleb@yahoo.com>
2021-09-08 21:41:40 +0500

    + method for download file;

--- grpc-server/src/main/java/rpc/ImageServiceImpl.java
+++ grpc-server/src/main/java/rpc/ImageServiceImpl.java
@@ -2,21 +2,22 @@ package rpc;
 
 import a.gleb.service.Image;
 import a.gleb.service.ImageServiceGrpc;
+import com.google.protobuf.ByteString;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.attribute.FileTime;
+import java.util.Arrays;
 
 public class ImageServiceImpl extends ImageServiceGrpc.ImageServiceImplBase {
 
     private final int mStatus = 200;
     private final String message = "ImageUpload: DONE";
     private BufferedOutputStream bufferedOutputStream = null;
+    private static final String SERVER_DIRECTORY = "D:\\GrpcServerCache\\";
 
     @Override
     public StreamObserver<Image.ImageRequest> uploadImage(StreamObserver<Image.ImageResponse> responseObserver) {
@@ -29,7 +30,7 @@ public class ImageServiceImpl extends ImageServiceGrpc.ImageServiceImplBase {
                 System.out.println(name);
                 try {
                     if (bufferedOutputStream == null) {
-                        bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("D:\\GrpcServerCache\\" + name));
+                        bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(SERVER_DIRECTORY + name));
                     }
                     bufferedOutputStream.write(data);
                     bufferedOutputStream.flush();
@@ -94,5 +95,31 @@ public class ImageServiceImpl extends ImageServiceGrpc.ImageServiceImplBase {
         }
     }
 
-    //TODO: download file from server;
+    @Override
+    public void downloadFile(Image.DownloadFileRequest request, StreamObserver<Image.DownloadFileResponse> responseObserver) {
+        try{
+            String filename = request.getFilename();
+            InputStream inputStream = new FileInputStream(SERVER_DIRECTORY + filename);
+            byte[] bytes = inputStream.readAllBytes();
+            BufferedInputStream imageStream = new BufferedInputStream(new ByteArrayInputStream(bytes));
+
+            int bufferSize = 1024;
+            byte[] buffer = new byte[bufferSize];
+            int length;
+            while ((length = imageStream.read(buffer, 0, bufferSize) ) != -1) {
+                responseObserver.onNext(Image.DownloadFileResponse
+                        .newBuilder()
+                        .setData(ByteString.copyFrom(buffer, 0, length))
+                        .setSize(bufferSize)
+                        .build());
+            }
+            imageStream.close();
+            responseObserver.onCompleted();
+        }catch (Throwable e){
+            responseObserver.onError(Status.ABORTED
+                    .withDescription("DownloadFileFromGrpc: occurred error " + request.getFilename())
+                    .withCause(e)
+                    .asException());
+        }
+    }
 }
--- web-grpc-client/src/main/java/a/gleb/webgrpcclient/service/ImageServiceImpl.java
+++ web-grpc-client/src/main/java/a/gleb/webgrpcclient/service/ImageServiceImpl.java
@@ -10,26 +10,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 @Slf4j
 @Service
 public class ImageServiceImpl {
 
-   private final ImageServiceGrpc.ImageServiceStub asyncStub;
-   private final ImageServiceGrpc.ImageServiceBlockingStub blockingStubImage;
+    private final ImageServiceGrpc.ImageServiceStub asyncStub;
+    private final ImageServiceGrpc.ImageServiceBlockingStub blockingStubImage;
 
-   @Autowired
+    @Autowired
     public ImageServiceImpl(ImageServiceGrpc.ImageServiceStub asyncStub, ImageServiceGrpc.ImageServiceBlockingStub blockingStubImage) {
         this.asyncStub = asyncStub;
         this.blockingStubImage = blockingStubImage;
     }
 
-    public void sendImage(final InputStream fileInputStream, String filename){
+    public void sendImage(final InputStream fileInputStream, String filename) {
         StreamObserver<Image.ImageResponse> responseStreamObserver = new StreamObserver<Image.ImageResponse>() {
             @Override
             public void onNext(Image.ImageResponse value) {
@@ -48,46 +52,91 @@ public class ImageServiceImpl {
         };
 
         StreamObserver<Image.ImageRequest> requestObserver = asyncStub.uploadImage(responseStreamObserver);
-        try{
+        try {
             System.out.println(filename);
-            try{
+            try {
                 BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
                 int buffer_size = 512 * 1024;
                 byte[] buffer = new byte[buffer_size];
                 int size = 0;
 
-                while ((size = bufferedInputStream.read(buffer)) > 0){
+                while ((size = bufferedInputStream.read(buffer)) > 0) {
                     ByteString byteString = ByteString.copyFrom(buffer, 0, size);
                     var request = Image.ImageRequest.newBuilder().setName(filename).setData(byteString).setOffset(size).build();
                     requestObserver.onNext(request);
                 }
-            }catch (IOException e){
+            } catch (IOException e) {
                 log.warn(e.getMessage());
             }
-        }catch (RuntimeException ex){
+        } catch (RuntimeException ex) {
             log.warn(ex.getMessage());
         }
         requestObserver.onCompleted();
     }
 
 
-    public List<FileInformation> checkServerDirectory(final String message){
-       List<FileInformation> fileInformationList = new ArrayList<>();
+    public List<FileInformation> checkServerDirectory(final String message) {
+        List<FileInformation> fileInformationList = new ArrayList<>();
         Image.FileRequest request = Image.FileRequest
                 .newBuilder()
                 .setMessage(message)
                 .build();
         Image.FileResponse response = blockingStubImage.checkFiles(request);
-        for (int count = 0; count < response.getFileInfoCount(); count++){
+        for (int count = 0; count < response.getFileInfoCount(); count++) {
             Image.FileInfo fileInfo = response.getFileInfo(count);
             FileInformation fileInformation = new FileInformation(fileInfo.getFilename(),
                     fileInfo.getFiletype(), fileInfo.getDateCreation());
             fileInformationList.add(fileInformation);
         }
-        for (FileInformation inf : fileInformationList){
-            System.out.println(inf);
-        }
         return fileInformationList;
+    }
+
+    public ByteArrayOutputStream downloadFile(final String filename) {
+        final ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        final AtomicBoolean completed = new AtomicBoolean(false);
+
+        StreamObserver<Image.DownloadFileResponse> streamObserver = new StreamObserver<Image.DownloadFileResponse>() {
+            @Override
+            public void onNext(Image.DownloadFileResponse dataResponse) {
+                try {
+                    byteArrayOutputStream.write(dataResponse.getData().toByteArray());
+                } catch (IOException e) {
+                    log.warn(e.getMessage());
+                }
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                log.warn("DownloadFileGrpc: server error: ", t);
+                countDownLatch.countDown();
+            }
+
+            @Override
+            public void onCompleted() {
+                log.info("DownloadFileGrpc: completed!");
+                completed.compareAndSet(false, true);
+                countDownLatch.countDown();
+            }
+        };
+        try{
+            Image.DownloadFileRequest request = Image.DownloadFileRequest
+                    .newBuilder()
+                    .setFilename(filename)
+                    .build();
+
+            asyncStub.downloadFile(request, streamObserver);
+
+            countDownLatch.await(5, TimeUnit.MINUTES);
+
+            if (!completed.get()){
+                throw  new Exception("DownloadFileGrpc: does not complete");
+            }
+        }catch (Exception e){
+            log.warn("DownloadFileGrpc: does not complete" + e.getMessage());
+        }
+
+        return byteArrayOutputStream;
     }
 
 }
